ee0549d86d6a7885c740fcaa737304edc692c975
Alex Yakovenko <uemon2005@gmail.com>
2017-11-13 19:36:32 +0200

    New files

---
+++ YBP.Framework/ActionExecResult.cs
@@ -0,0 +1,9 @@
+﻿namespace YBP.Framework
+{
+    public class ActionExecResult<TResult>
+    {
+        public int InstanceId { get; set; }
+        public int ActionId { get; internal set; }
+        public TResult Result { get; set; }
+    }
+}
---
+++ YBP.Framework/Regisry/YbpConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YBP.Framework.Regisry
+{
+    public class YbpConfiguration
+    {
+        public static Dictionary<Type, List<Type>> Actions => new Dictionary<Type, List<Type>>();
+
+        public static void LoadActionsFromAssembly<TTypeFromAssembly>()
+        {
+            var assembly = typeof(TTypeFromAssembly).Assembly;
+
+            var i = typeof(YbpActionBase<,,>);
+
+            var actions = assembly
+                .GetTypes()
+                .Where(x => i.IsAssignableFrom(x) && x.IsClass && !x.IsAbstract && x.IsPublic)
+                .GroupBy(x => x.BaseType.GenericTypeArguments[0])
+                .Select(x => new { x.Key, Vals = x.ToList() })
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Vals
+                );
+
+            foreach (var item in actions)
+            {
+                Actions[item.Key] = Actions[item.Key] ?? new List<Type>();
+                Actions[item.Key].AddRange(item.Value);
+            }
+        }
+
+    }
+
+}
